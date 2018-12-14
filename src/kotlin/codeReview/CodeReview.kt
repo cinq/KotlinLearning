ff8594e92ebba7d3e5a49b55df52b791040a0e74
mcharette <mcharette@kinaxis.com>
2018-12-14 11:10:58 -0500

    Trying a code review with a pull request

---
+++ src/kotlin/codeReview/CodeReview.kt
@@ -0,0 +1,8 @@
+package codeReview
+
+fun main(args: Array<String>) {
+    val title = "code review 01"
+    println("Title: $title")
+    println()
+    println("Trying a code review through a pull request.")
+}

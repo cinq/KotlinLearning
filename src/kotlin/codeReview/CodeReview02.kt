cfc932a536e29ce975d04c3f3520adaa4ba485fd
mcharette <mcharette@kinaxis.com>
2018-12-14 11:25:39 -0500

    Trying a different way to create a code review in my IDE

---
+++ src/kotlin/codeReview/CodeReview02.kt
@@ -0,0 +1,8 @@
+package codeReview
+
+fun main(args: Array<String>) {
+    val title = "Code Review 02"
+    println("Title: $title")
+    println()
+    println("Trying a different way in my IDE to create this code review.")
+}
